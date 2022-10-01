748a7c65deeb9af99e525314d88db9c3b968a3f4
Krisna Sirait <krisnapande00@outlook.com>
2022-10-01 18:16:17 +0700

    changes fragment 2 to VignereCipherFragment.kt

--- app/src/main/java/com/kingtech/cryptography/VignereCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/VignereCipherFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Algorithm2 : Fragment() {
+class VignereCipherFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_algorithm2, container, false)
+        return inflater.inflate(R.layout.fragment_vignere_cipher, container, false)
     }
 }
