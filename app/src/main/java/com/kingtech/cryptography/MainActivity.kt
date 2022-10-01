4369389730c8b832ef8479b4fe50645ceb170e18
Krisna Sirait <krisnapande00@outlook.com>
2022-10-01 11:47:09 +0700

    implement view bindinbg on MainActivity.kt

--- app/src/main/java/com/kingtech/cryptography/MainActivity.kt
+++ app/src/main/java/com/kingtech/cryptography/MainActivity.kt
@@ -3,13 +3,19 @@ package com.kingtech.cryptography
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.kingtech.cryptography.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
