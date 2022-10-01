58f9e11893fe2d9ae0525d9c0b93e77bdf408210
Krisna Sirait <krisnapande00@outlook.com>
2022-10-01 19:25:31 +0700

    view binding for CaesarCipherFragment.kt

--- app/src/main/java/com/kingtech/cryptography/CaesarCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/CaesarCipherFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kingtech.cryptography.databinding.FragmentCaesarCipherBinding
 
 class CaesarCipherFragment : Fragment() {
+
+    private lateinit var binding: FragmentCaesarCipherBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_caesar_cipher, container, false)
+        binding = FragmentCaesarCipherBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
