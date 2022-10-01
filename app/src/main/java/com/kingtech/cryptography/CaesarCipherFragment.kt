f111560108a787c199a986c73c91a2169ba00869
Krisna Sirait <krisnapande00@outlook.com>
2022-10-01 20:23:25 +0700

    problem in input string

--- app/src/main/java/com/kingtech/cryptography/CaesarCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/CaesarCipherFragment.kt
@@ -1,11 +1,12 @@
 package com.kingtech.cryptography
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.kingtech.cryptography.databinding.FragmentCaesarCipherBinding
+
 
 class CaesarCipherFragment : Fragment() {
 
@@ -14,9 +15,34 @@ class CaesarCipherFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentCaesarCipherBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnEncrypt.setOnClickListener {
+            val key = binding.etShiftKey.toString().toInt()
+            val encrypted = encrypt(binding.etInput.toString(), key)
+            binding.tvResult.text = encrypted
+        }
+    }
+
+    private fun encrypt(message:String, step:Int) : String {
+        val convMessage = message.lowercase()
+        val letters = "abcdefghijklmnopqrstuvwxyz"
+        val translated = mutableListOf<String>()
+        val len = convMessage.length - 1
+        for (i in 0..len) {
+            if (convMessage[i] in letters) {
+                translated.add(i, letters[(letters.indexOf(convMessage[i]) + step) % 26].toString())
+            } else {
+                translated.add(i, convMessage[i].toString())
+            }
+        }
+        return translated.joinToString("")
     }
 }
