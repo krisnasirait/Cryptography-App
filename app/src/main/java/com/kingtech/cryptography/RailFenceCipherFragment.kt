6e1eb372ab11a739d8303d07d8a614b043afdbcc
Krisna Sirait <krisnapande00@outlook.com>
2022-10-01 18:21:26 +0700

    changes fragment name and menu label

--- app/src/main/java/com/kingtech/cryptography/RailFenceCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/RailFenceCipherFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Algorithm3 : Fragment() {
+class RailFenceCipherFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_algorithm3, container, false)
+        return inflater.inflate(R.layout.fragment_rail_fence_cipher, container, false)
     }
 }
