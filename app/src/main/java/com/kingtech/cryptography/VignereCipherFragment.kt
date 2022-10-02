ca2ccf8e81434d459f5ec3de29eb564b3fff914a
Krisna Sirait <krisnapande00@outlook.com>
2022-10-02 21:51:07 +0700

    added layout

--- app/src/main/java/com/kingtech/cryptography/VignereCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/VignereCipherFragment.kt
@@ -1,35 +1,26 @@
 package com.kingtech.cryptography
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kingtech.cryptography.databinding.FragmentCaesarCipherBinding
+import com.kingtech.cryptography.databinding.FragmentVignereCipherBinding
 import java.util.*
 
 
 class VignereCipherFragment : Fragment() {
+
+    private lateinit var binding: FragmentVignereCipherBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vignere_cipher, container, false)
-    }
-
-    fun encrypt(Message: String, Key: String): String? {
-        var message = Message
-        var eMessage = ""
-        message = Message.uppercase(Locale.getDefault())
-        var i = 0
-        var j = 0
-        while (i < Message.length) {
-            val letter = Message[i]
-            eMessage += ((letter.code - 65 + (Key[j].code - 65)) % 26 + 65).toChar()
-            j = ++j % Key.length
-            i++
-        }
-        return eMessage
+        binding = FragmentVignereCipherBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
