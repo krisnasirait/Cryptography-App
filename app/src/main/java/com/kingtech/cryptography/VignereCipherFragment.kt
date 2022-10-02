4d97fb3646dd0ab8d9969082462f4fa1e1511121
Krisna Sirait <krisnapande00@outlook.com>
2022-10-02 20:26:04 +0700

    add function to encrypt

--- app/src/main/java/com/kingtech/cryptography/VignereCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/VignereCipherFragment.kt
@@ -1,10 +1,12 @@
 package com.kingtech.cryptography
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import java.util.*
+
 
 class VignereCipherFragment : Fragment() {
 
@@ -14,5 +16,20 @@ class VignereCipherFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vignere_cipher, container, false)
+    }
+
+    fun encrypt(Message: String, Key: String): String? {
+        var message = Message
+        var eMessage = ""
+        message = Message.uppercase(Locale.getDefault())
+        var i = 0
+        var j = 0
+        while (i < Message.length) {
+            val letter = Message[i]
+            eMessage += ((letter.code - 65 + (Key[j].code - 65)) % 26 + 65).toChar()
+            j = ++j % Key.length
+            i++
+        }
+        return eMessage
     }
 }
