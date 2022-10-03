2f6fbf3680df2f6967d0b9b40f2f7bba6a38480f
Krisna Sirait <krisnapande00@outlook.com>
2022-10-03 15:24:25 +0700

    added AES encryption

--- app/src/main/java/com/kingtech/cryptography/fragment/RailFenceCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/fragment/RailFenceCipherFragment.kt
@@ -6,13 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kingtech.cryptography.R
+import com.kingtech.cryptography.databinding.FragmentRailFenceCipherBinding
 
 class RailFenceCipherFragment : Fragment() {
+
+    private lateinit var binding: FragmentRailFenceCipherBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rail_fence_cipher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
