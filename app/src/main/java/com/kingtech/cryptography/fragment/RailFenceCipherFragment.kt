0a48bb175cc3e0be8d011ede7e81d70f4dc91a12
Krisna Sirait <krisnapande00@outlook.com>
2022-10-03 18:46:06 +0700

    working RSA

--- app/src/main/java/com/kingtech/cryptography/fragment/RailFenceCipherFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/fragment/RailFenceCipherFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import br.com.amorim.crypto.crypto.RSACrypto
+import br.com.amorim.crypto.keys.AesKey
 import com.kingtech.cryptography.R
+import com.kingtech.cryptography.databinding.FragmentCaesarCipherBinding
 import com.kingtech.cryptography.databinding.FragmentRailFenceCipherBinding
+import javax.crypto.Cipher
 
 class RailFenceCipherFragment : Fragment() {
 
@@ -17,10 +22,29 @@ class RailFenceCipherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rail_fence_cipher, container, false)
+        binding = FragmentRailFenceCipherBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val rsaCrypto = context?.let { RSACrypto(it) }
+
+        val textToEncrypt = view.findViewById<TextView>(R.id.etInput)
+        val textToDecrypt = view.findViewById<TextView>(R.id.etEncrypted)
+        val decryptResult = view.findViewById<TextView>(R.id.tvResult)
+
+        binding.btnEncrypt.setOnClickListener {
+            rsaCrypto?.encrypt(textToEncrypt.text.toString()).let {
+                textToDecrypt.text = it
+            }
+        }
+
+        binding.btnDecrypt.setOnClickListener {
+            rsaCrypto?.decrypt(textToDecrypt.text.toString()).let {
+                decryptResult.text = it
+            }
+        }
     }
 }
