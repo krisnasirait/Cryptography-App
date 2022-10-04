bfcb59e1ab1e702d66864eb2b60e01483e3d6cbe
Krisna Sirait <krisnapande00@outlook.com>
2022-10-04 19:22:00 +0700

    changes layout from tv to et in caesar cipher

--- app/src/main/java/com/kingtech/cryptography/fragment/SuperAlgorithmFragment.kt
+++ app/src/main/java/com/kingtech/cryptography/fragment/SuperAlgorithmFragment.kt
@@ -1,15 +1,15 @@
-package com.kingtech.cryptography
+package com.kingtech.cryptography.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kingtech.cryptography.R
 
-class SuperAlgorithm : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SuperAlgorithmFragment : Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
